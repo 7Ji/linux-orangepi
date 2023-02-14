324220bd2dfceaf66c8db95a6bfc3209e06b84f3
7Ji <pugokushin@gmail.com>
2023-02-14 19:57:32 +0800

    drivers/net/wireless/*: replace all apperance of 'if (&psta->lock != NULL)' with 'if (psta)'
      &psta->lock gets the address of psta->lock, while lock
      is the member of struct psta, this makes no sense as
      its address will always be non-NULL
    
      changing this to comparing psta directly with NULL
    
      this fixes the following build issue:
    
        drivers/net/wireless/rtl8811cu/core/rtw_sta_mgt.c: In function 'rtw_mfree_stainfo':
        drivers/net/wireless/rtl8811cu/core/rtw_sta_mgt.c:379:25: warning: the comparison will always evaluate as 'true' for the address of 'lock' will never be NULL [-Waddress]
        error, forbidden warning:rtw_sta_mgt.c:379
          379 |         if (&psta->lock != NULL)
              |                         ^~

--- drivers/net/wireless/rockchip_wlan/rtl8822be/core/rtw_sta_mgt.c
+++ drivers/net/wireless/rockchip_wlan/rtl8822be/core/rtw_sta_mgt.c
@@ -348,7 +348,7 @@ void rtw_mfree_stainfo(struct sta_info *psta)
 {
 	_func_enter_;
 
-	if (&psta->lock != NULL)
+	if (psta)
 		_rtw_spinlock_free(&psta->lock);
 
 	_rtw_free_sta_xmit_priv_lock(&psta->sta_xmitpriv);
